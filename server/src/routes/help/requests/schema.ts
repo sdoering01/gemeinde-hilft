a5e330d5e64d19dbc5905aa14f82e5b17b31906b
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2021-04-01 20:28:50 +0200

    Allow empty strings in schema

--- server/src/routes/help/requests/schema.ts
+++ server/src/routes/help/requests/schema.ts
@@ -3,14 +3,14 @@ import Joi from 'joi';
 export default {
     helpRequest: Joi.object().keys({
         email: Joi.string().required().max(200).email(),
-        name: Joi.string().max(100),
+        name: Joi.string().allow('').max(100),
         title: Joi.string().required().max(100),
-        description: Joi.string().max(500)
+        description: Joi.string().allow('').max(500)
     }),
     helpRequestEdit: Joi.object()
         .keys({
-            title: Joi.string().max(100),
-            description: Joi.string().max(500)
+            title: Joi.string().allow('').max(100),
+            description: Joi.string().allow('').max(500)
         })
         .or('title', 'description'),
     helpRequestToken: Joi.object()
